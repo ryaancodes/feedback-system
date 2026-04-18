a44841179ed449b5b6caaf441e0a7360ea21e1f4
Ryaan Devnani <ryaandevnani@gmail.com>
2026-04-19 03:51:54 +0530

    removed remote DB, using local DB

--- config/db.js
+++ config/db.js
@@ -1,11 +1,17 @@
 const { Pool } = require('pg');
 
+const { Pool } = require('pg');
+
 const pool = new Pool({
-  connectionString: 'postgresql://feedback_db_avd4_user:6DsXQ9Y3eBtab0sgn9WcBDpd7YAweHee@dpg-d7f7qahkh4rs739ibbtg-a.virginia-postgres.render.com/feedback_db_avd4',
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL
 });
+
+module.exports = {
+  execute: async (query, params = []) => {
+    const res = await pool.query(query, params);
+    return res.rows;
+  }
+};
 
 module.exports = {
   execute: async (query, params = []) => {
